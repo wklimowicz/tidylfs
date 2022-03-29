aec81882a98c95da8114cd0fc112e0ef4dee5fb4
Wojtek Klimowicz <wojtekklimowicz96@gmail.com>
2022-03-29 16:14:26 +0100

    Test data.table

--- inst/extra-tests/test-longitudinal.R
+++ inst/extra-tests/test-longitudinal.R
@@ -1,19 +1,77 @@
-library(tidyverse)
+library(data.table)
+library(tictoc)
 load_all()
 
-lfs <- lfs %>%
-    arrange(QUARTER) %>%
-    group_by(CASENO) %>%
-    mutate(last_job = lag(OCCUPATION)) %>%
-    ungroup()
+# lfs_compile("../lfs_rds_data/")
 
-lfs %>%
-    filter(last_job == 2317) %>%
-    count(OCCUPATION_DESCRIPTION, sort = T)
+server_dir <- "//vmt1pr-spss2a/LFS/Master/Quarterly datasets/2004 onwards"
+latest_quarter_filepath <- "//vmt1pr-spss2a/LFS/Master/Quarterly datasets/2004 onwards/2021 Q4.sav"
 
-library(data.table)
+list.files(server_dir)
 
-lfsdt <- data.table(lfs)
+list.files(".")
+
+lfs_convert(, "../lfs_raw_from_server")
+
+lfs <- lfs_load()
+
+lfs <- data.table(lfs)
+
+setkey(lfs, YEAR, CASENO)
+
+lfs
+
+tic()
+lfs[, last_job := c(NA, OCCUPATION_DESCRIPTION[-.N]), by = CASENO]
+toc()
+
+tic()
+lfs[, last_job := shift(OCCUPATION_DESCRIPTION, 1), by = CASENO]
+toc()
+
+lfs[, .N, by = last_job]
+
+lfs[OCCUPATION_DESCRIPTION == "Head teachers and principals"]
+
+lfs[CASENO == " 96013040610101"]
+lfs[CASENO == " 10711010210102"]
+
+
+lfs[, .N , by =OCCUPATION_DESCRIPTION][order(-N)]
+
+lfs["Head teachers and principals", .N, on = "OCCUPATION_DESCRIPTION", by = YEAR]
+lfs["Senior professionals of educational establishments", .N, on = "OCCUPATION_DESCRIPTION", by = YEAR]
+lfs["Registrars and senior administrators of educational establishments", .N, on = "OCCUPATION_DESCRIPTION", by = YEAR]
+
+leader_descriptions <- c("Head teachers and principals",
+"Senior professionals of educational establishments",
+"Registrars and senior administrators of educational establishments")
+
+
+lfs[, leader := OCCUPATION_DESCRIPTION %in% leader_descriptions & !is.na(CASENO)] 
+
+lfs[, last_job_leader := shift(OCCUPATION_DESCRIPTION, 1) %in% leader_descriptions & !is.na(CASENO)] 
+
+
+lfs[, .N, by = .(YEAR, leader_ever)]
+
+
+lfs[, leader_ever := max(leader == 1), by = CASENO]
+
+
+lfs[YEAR > 2010 & leader_ever == 1, .N, by = OCCUPATION_DESCRIPTION][order(-N)] 
+
+
+lfs[YEAR > 2010 & last_job_leader == 1, .N, by = OCCUPATION_DESCRIPTION][order(-N)]  %>%
+  sie()
+
+
+
+lfs[last_job == 2317 & YEAR %in% c(2010:2020), .N, by = OCCUPATION_DESCRIPTION]
+
+lfs[leader == 1, .N, by = OCCUPATION_DESCRIPTION]
+
+
 
 library(tictoc)
 
@@ -23,9 +81,9 @@ lfs %>%
     summarise(n = n())
 toc()
 
-tic()
-x = lfsdt[, .N, by = CASENO][,.N, by = N][order(-N)]
-toc()
+# tic()
+lfs[, .N, by = CASENO][,.N, by = N][order(-N)]
+# toc()
 
 
 
